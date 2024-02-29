b74b91c7dfe2e5ee3d8b9d870544201605279610
Artur Pletnev <art-pletnev@mail.ru>
2024-02-29 12:55:18 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -64,3 +64,28 @@ console.log(extractNumber(-1));
 
 console.log(extractNumber(1.5));
 */
+
+// module5-task2
+
+const isMeetinginWorkingHours = (startWorkingTime, endWorkingTime, meetingStart, meetingTime) => {
+
+  const timeStringToMinutes = (timeString) => {
+    const [hours, minutes] = timeString.split(':').map(part => parseInt(part, 10));
+    return hours * 60 + minutes;
+  };
+
+  const startWorkingMinutes = timeStringToMinutes(startWorkingTime);
+  const endWorkingMinutes = timeStringToMinutes(endWorkingTime);
+  const meetingStartMinutes = timeStringToMinutes(meetingStart);
+
+  const meetingEndMinutes = meetingStartMinutes + meetingTime;
+
+  return meetingStartMinutes >= startWorkingMinutes && meetingEndMinutes <= endWorkingMinutes;
+};
+
+
+console.log(isMeetinginWorkingHours('08:00', '17:30', '14:00', 90)); // true
+console.log(isMeetinginWorkingHours('8:0', '10:0', '8:0', 120));     // true
+console.log(isMeetinginWorkingHours('08:00', '14:30', '14:00', 90)); // false
+console.log(isMeetinginWorkingHours('14:00', '17:30', '08:0', 90));  // false
+console.log(isMeetinginWorkingHours('8:00', '17:30', '08:00', 900)); // false
