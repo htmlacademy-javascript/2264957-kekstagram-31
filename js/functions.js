e4b402d9a205f15bd1bc36a4659440df5f8ea240
Artur Pletnev <art-pletnev@mail.ru>
2024-02-29 11:39:05 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -84,8 +84,8 @@ const isMeetinginWorkingHours = (workingStartTime, workingEndTime, meetingStartT
 
 };
 
-console.log(isMeetinginWorkingHours('08:00', '17:30', '14:00', 90)); // true
-console.log(isMeetinginWorkingHours('8:0', '10:0', '8:0', 120)); // true
-console.log(isMeetinginWorkingHours('08:00', '14:30', '14:00', 90)); // false
-console.log(isMeetinginWorkingHours('14:00', '17:30', '08:0', 90)); // false
-console.log(isMeetinginWorkingHours('8:00', '17:30', '08:00', 900)); // false
+console.log(isMeetinginWorkingHours('08:00', '17:30', '14:00', 90));
+console.log(isMeetinginWorkingHours('8:0', '10:0', '8:0', 120));
+console.log(isMeetinginWorkingHours('08:00', '14:30', '14:00', 90));
+console.log(isMeetinginWorkingHours('14:00', '17:30', '08:0', 90));
+console.log(isMeetinginWorkingHours('8:00', '17:30', '08:00', 900));
