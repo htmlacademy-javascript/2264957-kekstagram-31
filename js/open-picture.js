73e8da9e5bceda4766a97cd84123d6fbd7d9dfb2
Artur Pletnev <art-pletnev@mail.ru>
2024-03-22 21:25:41 +0300

    Правки

--- js/open-picture.js
+++ js/open-picture.js
@@ -6,10 +6,20 @@ const userModalClosePicture = userModalPicture.querySelector('.big-picture__canc
 const body = document.querySelector('body');
 const commentTemplate = userModalPicture.querySelector('.social__comment');
 const commentSection = userModalPicture.querySelector('.social__comments');
+const commentsCounts = userModalPicture.querySelector('social__comment-count');
 const commentsShowCount = userModalPicture.querySelector('.social__comment-shown-count');
 const commentsTotalCount = userModalPicture.querySelector('.social__comment-total-count');
 const commentsLoad = userModalPicture.querySelector('.comments-loader');
+const MIN_SHOW_COMMENTS = 5;
+let currentComments = [];
+let startCommentsCount = 0;
+let commentsForRender = [];
 
+
+const clearComments = () => {
+  startCommentsCount = 0;
+  commentSection.innerHTML = '';
+};
 
 const onPictureEscapeKeyDown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -19,22 +29,43 @@ const onPictureEscapeKeyDown = (evt) => {
 };
 
 const closeBigPicture = () => {
+  clearComments();
   userModalPicture.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPictureEscapeKeyDown);
 };
 
-
-const renderPictureComments = (comments) => {
-  comments.forEach(({ avatar, message }) => {
+const renderPictureComments = () => {
+  commentsForRender = currentComments.slice(startCommentsCount, startCommentsCount + MIN_SHOW_COMMENTS);
+  console.log(commentsForRender);
+  startCommentsCount += MIN_SHOW_COMMENTS;
+  const commentsFragment = document.createDocumentFragment();
+  commentsShowCount.textContent = commentsForRender.length;
+  commentsForRender.forEach(({ avatar, message }) => {
     const comment = commentTemplate.cloneNode(true);
     comment.querySelector('.social__picture').src = avatar;
     comment.querySelector('.social__text').textContent = message;
-    commentsShowCount.textContent = comments.length;
-    commentsTotalCount.textContent = comments.length;
-    commentSection.appendChild(comment);
+    commentsFragment.append(comment);
   });
+  commentSection.append(commentsFragment);
+  // if (comments.length < MIN_SHOW_COMMENTS) {
+  //   commentsShowCount.textContent = comments.length;
+  // } else {
+  //   commentsShowCount.textContent = MIN_SHOW_COMMENTS;
+  // }
+  // startCommentsCount += MIN_SHOW_COMMENTS;
+  // commentsShowCount.textContent = startCommentsCount;
+  if (commentsForRender.length === commentSection.children.length) {
+
+    commentsLoad.classList.add('hidden');
+  }
 };
+
+// const loadNextComments = (commentsArrey) => {
+//   commentsForRender = commentsArrey;
+//   renderPictureComments();
+//   commentsLoad.addEventListener('click', renderPictureComments());
+// };
 
 const renderBigPicture = ({url, description, likes, comments }) => {
   userModalPicture.querySelector('.big-picture__img').querySelector('img').src = url;
@@ -42,18 +73,22 @@ const renderBigPicture = ({url, description, likes, comments }) => {
   userModalPicture.querySelector('.likes-count').textContent = likes;
   userModalPicture.querySelector('.likes-count').textContent = description;
   userModalPicture.querySelector('.social__caption').textContent = description;
-  renderPictureComments(comments);
+  commentsTotalCount.textContent = comments.length;
+  if (comments.length > MIN_SHOW_COMMENTS) {
+  commentsLoad.addEventListener('click', () => {
+    renderPictureComments();
+  });
+};
+  currentComments = comments;
+  renderPictureComments();
 };
 
 const showBigPicture = ({url, description, likes, comments }) => {
   userModalPicture.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onPictureEscapeKeyDown);
-  commentSection.innerHTML = '';
   renderBigPicture ({url, description, likes, comments });
-  commentsShowCount.classList.add('hidden');
-  commentsTotalCount.classList.add('hidden');
-  commentsLoad.classList.add('hidden');
+  //loadNextComments(comments);
   userModalClosePicture.addEventListener('click', () => {
     closeBigPicture();
   });
